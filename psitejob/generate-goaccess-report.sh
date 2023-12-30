1ce4da301733e4697477044d34708d14c311f4cd
Daniel <d@stuhrs.dk>
2023-12-30 11:59:51 +0100

    Fix paths

--- psitejob/generate-goaccess-report.sh
+++ psitejob/generate-goaccess-report.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-declare -r output_dir="/usr/local/psitejob/goaccess/"
-declare -r log_dir="/var/log/stuhrs_dk/web/"
+declare -r output_dir="/usr/local/psitejob/goaccess"
+declare -r log_dir="/var/log/stuhrs_dk/web"
 
 echo "$(date) - Generating GoAccess report..."
 goaccess -o "${output_dir}/index.html" -f "${log_dir}/access.log" "${log_dir}/access.log.1" --log-format=COMBINED
